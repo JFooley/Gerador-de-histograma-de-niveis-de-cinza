2233717031b201d03fb58115c485c72898dde05f
João Gabriel <gabriel.lima.almeida@gmail.com>
2023-08-30 12:12:35 -0300

    first commit

---
+++ leitor-niveis-de-cinza.py
@@ -0,0 +1,50 @@
+from PIL import Image
+import os
+
+def agruparPixels(pixelsDesagrupados):
+    agrupados = {}
+
+    for i in range(0, 255, 1):
+        agrupados[f'{i}'] = 0
+    
+    for pixel in pixelsDesagrupados:
+        agrupados[f'{pixel}'] += 1
+
+    return agrupados
+
+def lerPixels():
+    print('Iniciando leitura')
+    
+    lista = []
+    for y in range(altura):
+        for x in range(largura):
+            pixel = imagem.getpixel((x, y))
+            valor_r = pixel[0]
+            valor_g = pixel[1]
+            valor_b = pixel[2]
+
+            pixel = (valor_r + valor_g + valor_b) // 3
+
+            lista.append(pixel)
+            
+        if y == altura / 4:
+            print('25%')
+        elif y == altura / 2:
+            print('50%')
+        elif y == (altura * 3) / 4:
+            print('75%')
+
+    print('Concluído')
+    
+    return lista
+
+imagem = Image.open('imagem.jpg')
+largura, altura = imagem.size
+
+pixels = lerPixels()
+pixels.sort()
+
+pixelsAgrupados = agruparPixels(pixels)
+
+print(pixelsAgrupados)
+
